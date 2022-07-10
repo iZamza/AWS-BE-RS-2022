ffef6ceb508dd64872ae2af1cf8039b87aba9c35
Viktar Tsikhmanovich <Viktar_Tsikhmanovich@epam.com>
2022-07-10 18:54:50 +0200

    task 5: uncommit files

--- import-service/import-file-parser.js
+++ import-service/import-file-parser.js
@@ -1,34 +1,34 @@
-// import AWS from "aws-sdk";
-// const BUCKET = "my-import-service";
-// import csv from "csv-parser";
-//
-// const importFileParser = async (event) => {
-//   console.log(event);
-//   const s3 = new AWS.S3({ region: "eu-west-1" });
-//   for (const record of event.Records) {
-//     const Key = record.s3.object.key;
-//     const bucketName = record.s3.bucket.name;
-//
-//     const bucket = s3.getObject({
-//       Bucket: BUCKET,
-//       Key
-//     });
-//
-//     const handleParseAndUpdate = async () => {
-//       await s3.copyObject({ Bucket: bucketName, Key: Key.replace("uploaded", "parsed"), CopySource: bucketName + "/" + Key }).promise();
-//       await s3.deleteObject({ Bucket: bucketName, Key }).promise();
-//     };
-//
-//     await (() => {
-//       return new Promise(() => {
-//         bucket.createReadStream().pipe(csv()).on("end", handleParseAndUpdate);
-//       })
-//     });
-//   }
-//   return {
-//     statusCode: 200, body: JSON.stringify({ message: "success" })
-//   };
-// };
-//
-//
-// export default importFileParser;
+import AWS from "aws-sdk";
+const BUCKET = "my-import-service";
+import csv from "csv-parser";
+
+const importFileParser = async (event) => {
+  console.log(event);
+  const s3 = new AWS.S3({ region: "eu-west-1" });
+  for (const record of event.Records) {
+    const Key = record.s3.object.key;
+    const bucketName = record.s3.bucket.name;
+
+    const bucket = s3.getObject({
+      Bucket: BUCKET,
+      Key
+    });
+
+    const handleParseAndUpdate = async () => {
+      await s3.copyObject({ Bucket: bucketName, Key: Key.replace("uploaded", "parsed"), CopySource: bucketName + "/" + Key }).promise();
+      await s3.deleteObject({ Bucket: bucketName, Key }).promise();
+    };
+
+    await (() => {
+      return new Promise(() => {
+        bucket.createReadStream().pipe(csv()).on("end", handleParseAndUpdate);
+      })
+    });
+  }
+  return {
+    statusCode: 200, body: JSON.stringify({ message: "success" })
+  };
+};
+
+
+export default importFileParser;
--- import-service/import-products-file.js
+++ import-service/import-products-file.js
@@ -1,24 +1,24 @@
-// import AWS from "aws-sdk";
-// const BUCKET = "import-service";
-//
-// const importProductsFile = async (event) => {
-//   console.log(event);
-//   const s3 = new AWS.S3({ region: "eu-west-1" });
-//   const { name } = event.queryStringParameters;
-//   const command = {
-//     Bucket: BUCKET,
-//     Key: `uploaded/${name}`,
-//     ContentType: "text/csv",
-//     Expires: 1000,
-//   };
-//
-//   const signedUrl = s3.getSignedUrl("putObject", command);
-//
-//   return {
-//     statusCode: 201,
-//     headers: { "Access-Control-Allow-Origin": "*" },
-//     body: JSON.stringify(signedUrl),
-//   };
-// };
-//
-// export default importProductsFile;
+import AWS from "aws-sdk";
+const BUCKET = "import-service";
+
+const importProductsFile = async (event) => {
+  console.log(event);
+  const s3 = new AWS.S3({ region: "eu-west-1" });
+  const { name } = event.queryStringParameters;
+  const command = {
+    Bucket: BUCKET,
+    Key: `uploaded/${name}`,
+    ContentType: "text/csv",
+    Expires: 1000,
+  };
+
+  const signedUrl = s3.getSignedUrl("putObject", command);
+
+  return {
+    statusCode: 201,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify(signedUrl),
+  };
+};
+
+export default importProductsFile;
